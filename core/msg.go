de67074bf011f0797e7f0f4486a41cd52e54cedf
ZCS <zcsk18@qq.com>
2021-07-22 16:28:10 +0800

    1

--- core/msg.go
+++ core/msg.go
@@ -2,14 +2,8 @@ package core
 
 import (
 	"net"
-	"strconv"
 	"sync"
 )
-
-type Msg struct {
-	Action string
-	Data []byte
-}
 
 var Pool = &sync.Pool{
 	New: func() interface{} {
@@ -17,69 +11,18 @@ var Pool = &sync.Pool{
 	},
 }
 
-func ProxySend(c net.Conn, action string, data []byte) error {
-	buff := make([]byte, 512)
 
-	length := len(data)
-	copy(buff, IntToBytes(length))
-	_, err := c.Write(buff[:strconv.IntSize]);
+func ProxyRead(c net.Conn, b []byte) (n int, err error) {
+	n,err = c.Read(b)
 	if err != nil {
-		return err
+		return n, err
 	}
 
-	copy(buff, action);
-	_, err = c.Write(buff[:1]);
-	if err != nil {
-		return err
-	}
-
-	//Encode(&data, len(data))
-
-	totle := 0
-	for {
-		n, err := c.Write(data[totle:]);
-		if err != nil {
-			return err
-		}
-		totle += n
-		if (totle == len(data)) {
-			break
-		}
-	}
-
-	return nil
+	return n, err
 }
 
 
-func ProxyRecv(c net.Conn) (*Msg, error){
-	buff_len := make([]byte, strconv.IntSize)
-	buff_op := make([]byte, 1)
-	buff_data := make([]byte, 1024*100)
-	msg := Msg{}
-	length, err := c.Read(buff_len);
-	if err != nil {
-		return &msg, err;
-	}
-	if length <= 0 {
-		return &msg, err;
-	}
-
-	length, err = c.Read(buff_op);
-	if err != nil {
-		return &msg, err;
-	}
-	if length <= 0 {
-		return &msg, err;
-	}
-	msg.Action = string(buff_op)
-
-	length, err = c.Read(buff_data);
-	if err != nil {
-		return &msg, err;
-	}
-
-	//Decode(&buff_data, length)
-	msg.Data = buff_data[:length]
-
-	return &msg, nil
+func ProxyWrite(c net.Conn, b []byte) (n int, err error) {
+	n, err = c.Write(b)
+	return n, err
 }
